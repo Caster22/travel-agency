055377bd9bac8ad0fa9716952c00aea971671a6d
Michał Kopacz <kopaczmichal093@gmail.com>
2020-07-26 00:43:20 +0200

    Add test to HappyHourAd.test

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const getCountdownTime = () => {
-  const currentTime = new Date();
-  const nextNoon = new Date(Date.UTC(currentTime.getUTCFullYear(), currentTime.getUTCMonth(), currentTime.getUTCDate(), 12, 0, 0, 0));
-
-  if(currentTime.getUTCHours() >= 12){
-    nextNoon.setUTCDate(currentTime.getUTCDate()+1);
+class HappyHourAd extends React.Component {
+  static propTypes = {
+    title: PropTypes.string,
   }
-  return Math.round((nextNoon.getTime() - currentTime.getTime())/1000);
-};
 
-// eslint-disable-next-line react/prop-types
-const HappyHourAd = ({ title }) => (
-  <div className="countdown">
-    <h3 className="title">{title}</h3>
-    <div className='promoDescription'>{getCountdownTime()}</div>
-  </div>
-);
+  getCountdownTime(){
+    const currentTime = new Date();
+    const nextNoon = new Date(Date.UTC(currentTime.getUTCFullYear(),
+      currentTime.getUTCMonth(), currentTime.getUTCDate(),
+      12, 0, 0, 0));
 
-HappyHourAd.prototypes = {
-  title: PropTypes.string,
-};
+    if(currentTime.getUTCHours() >= 12){
+      nextNoon.setUTCDate(currentTime.getUTCDate()+1);
+    }
+
+    return Math.round((nextNoon.getTime() - currentTime.getTime())/1000);
+  }
+
+  constructor() {
+    super();
+    setInterval(() => this.forceUpdate(), 1000);
+  }
+  render() {
+    const { title } = this.props;
+    return (
+      <div className="countdown">
+        <h3 className="title">{title}</h3>
+        <div className='promoDescription'>{this.getCountdownTime()}</div>
+      </div>
+    );
+  }
+}
 
 export default HappyHourAd;
